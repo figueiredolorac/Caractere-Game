034c314a174b253cfdb5e3cc7520e63d060e270b
figueiredolorac <62906496+figueiredolorac@users.noreply.github.com>
2020-06-16 11:33:13 -0300

    Add files via upload

---
+++ public/js/frase.js
@@ -0,0 +1,49 @@
+$("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
+
+function fraseAleatoria() {
+    $("#spinner").show();
+
+    $.get("http://localhost:3000/frases",trocaFrase)
+    .fail(function(){
+        $("#erro").show();
+        setTimeout(function(){
+            $("#erro").toggle();
+        },2500);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data){
+    var frase = $(".frase");
+    var numAleatorio = Math.floor(Math.random() * data.length);
+    frase.text(data[numAleatorio].texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data[numAleatorio].tempo);
+}
+
+function buscaFrase(){
+    $("#spinner").show();
+    var fraseId = $("#frase-id").val();
+    var dados = { id: fraseId};
+
+    $.get("http://localhost:3000/frases",dados,trocaFrase)
+    .fail(function(){
+        $("#erro").show();
+        setTimeout(function(){
+            $("#erro").toggle();
+        },2500);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data){
+    var frase = $(".frase");
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
+}
